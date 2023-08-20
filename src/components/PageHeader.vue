fc6969bac93b80657aad94b12bf39e0e61dba6bb
essential14 <essential193@gmail.com>
2023-08-20 21:38:36 +0900

    [FEAT] 세션 id 값을 이용한 nav 상태 로직 추가 (수정 필요/login api 로직 수정 필요)

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,11 +1,11 @@
-<!-- PageHeader.vue -->
 <template>
   <header>
     <div id="nav">
-      <router-link to="/"> Home </router-link> |
-      <router-link to="/board/list"> 커뮤니티 </router-link> |
-      <router-link to="/page/login"> 로그인 </router-link> |
-      <router-link to="/page/join"> 회원가입 </router-link>
+      <router-link to="/"> Home |</router-link>
+      <router-link to="/board/list"> 커뮤니티 |</router-link>
+      <router-link to="/page/login" v-show="!isLoggedIn"> 로그인 |</router-link>
+      <a href="#" v-show="isLoggedIn" @click="handleLogout"> 로그아웃 |</a>
+      <router-link to="/page/join" v-show="!isLoggedIn"> 회원가입 </router-link>
       {{ this.$store.state.id }}
     </div>
   </header>
@@ -13,7 +13,25 @@
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["id"]),
+    isLoggedIn() {
+      return (
+        sessionStorage.getItem("id") !== null &&
+        sessionStorage.getItem("id") !== ""
+      );
+    },
+  },
+  methods: {
+    handleLogout() {
+      sessionStorage.removeItem("id");
+      this.$router.push("/page/login");
+    },
+  },
+};
 </script>
 
 <style scoped></style>
